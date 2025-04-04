1b90950b66f36a9e60efea4cd670708ed7655cb3
mattking999 <matt.king99@outlook.com>
2025-04-04 16:47:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -338,7 +338,7 @@ function createTestingBanner() {
     banner.style.textAlign = "center";
     banner.style.padding = "10px";
     banner.style.zIndex = "1000"; // Make sure it's on top of other elements
-    banner.innerHTML = "By continuing to use this website, you acknowledge and agree to the disclaimer at the bottom of this page. AWIN";
+    banner.innerHTML = "By continuing to use this website, you acknowledge and agree to the disclaimer at the bottom of this page.";
 
     document.body.appendChild(banner);
 }
